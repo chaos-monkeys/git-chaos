0a971c6ce48d1c16cea57f2828cc506b5ad68a9c
Ollie Williams <ollie.williams@ibm.com>
2019-10-15 19:52:35 +0100

    now in go!

--- packages/backend/lambda/src/hello.go
+++ packages/backend/lambda/src/hello.go
@@ -8,7 +8,7 @@ import (
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, Lambda",
+		Body:       "Go, Lambda",
 	}, nil
 }
 
--- packages/backend/lambda/src/hello.js
+++
@@ -1,8 +0,0 @@
-// FORCE A DEPLOY
-
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, Lambda"
-  });
-};
