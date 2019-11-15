e62775165abd6d0271c4a2b5633d6ce97593aa87
robalaban <rob.balaban@gmail.com>
2019-11-15 09:53:18 -0800

    Add comment on HealthCheck function

--- packages/backend/app/app.go
+++ packages/backend/app/app.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// HealthCheckHandler provides a simple up / down api of the service
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
