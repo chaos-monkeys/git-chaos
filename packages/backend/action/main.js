7ba7d0f6e0dcf39e392620598117e21d23c07040
robalaban <rob.balaban@gmail.com>
2019-11-24 17:25:48 +0000

    Add PR number to env

--- packages/backend/action/main.js
+++ packages/backend/action/main.js
@@ -2,6 +2,9 @@ const core = require("@actions/core");
 const Octokit = require("@octokit/rest");
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const GIT_OWNER = process.env.GIT_OWNER;
+const GIT_REPO = process.env.GIT_REPO;
+const GITHUB_SHA = process.env.GITHUB_SHA;
 
 const octokit = new Octokit({
   auth: GITHUB_TOKEN
@@ -14,19 +17,38 @@ const checkRequiredEnv = () => {
   }
 };
 
-const run = () => {
+const getPullRequestNumber = () => {
+  const openPullRequest = octokit.pulls.list({
+    owner: GIT_OWNER,
+    repo: GIT_OWNER,
+    state: 'open'
+  }).catch(err => {
+    console.log(err)
+  })
+
+  openPullRequest.forEach(pullRequest => {
+    const pullRequestSHA = pullRequest.head.sha;
+    const pullRequestNumber = 0;
+    if (GITHUB_SHA === pullRequestSHA) {
+      pullRequestNumber = pullRequest.number
+    }
+    core.exportVariable('PR_NUMBER', pullRequestNumber);
+    return pullRequestNumber;
+  });
+}
+
+(function run() {
   checkRequiredEnv();
   const commentMessage = core.getInput("message");
+  const pullRequestNumber = getPullRequestNumber();
   octokit.issues
     .createComment({
-      owner: "chaos-monkeys",
-      repo: "git-chaos",
-      issue_number: 10,
+      owner: GIT_OWNER,
+      repo: GIT_OWNER,
+      issue_number: pullRequestNumber,
       body: commentMessage
     })
     .catch(err => {
       console.log(err);
     });
-};
-
-run();
+})();
