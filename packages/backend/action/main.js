ddfa15d7d6946e6a9d409e5f2e908bfc1aecad0e
robalaban <rob.balaban@gmail.com>
2019-11-24 15:10:58 +0000

    Testing

--- packages/backend/action/main.js
+++ packages/backend/action/main.js
@@ -9,29 +9,41 @@ const octokit = new Octokit({
   auth: GITHUB_TOKEN
 });
 
-const checkRequiredEnv = () => {
-  if (GITHUB_TOKEN === null && GITHUB_TOKEN === undefined) {
-    core.warning(`Variable check failed: GITHUB_TOKEN: ${GITHUB_TOKEN}`);
-    core.setFailed(error.message);
-  }
+// const checkRequiredEnv = () => {
+//   if (GITHUB_TOKEN === null && GITHUB_TOKEN === undefined) {
+//     core.warning(`Variable check failed: GITHUB_TOKEN: ${GITHUB_TOKEN}`);
+//     core.setFailed(error.message);
+//   }
+// };
+
+const run = () => {
+  const commentMessage = core.getInput("message");
+  octokit.pulls.createComment({
+    owner: "chaos-monkeys",
+    repo: process.env.GITHUB_REPOSITORY,
+    pull_number: 10,
+    body: commentMessage,
+    commit_id: process.env.GITHUB_SHA,
+    path: process.env.GITHUB_EVENT_PATH
+  });
 };
 
-async function run() {
-  checkRequiredEnv();
-  try {
-    const commentMessage = core.getInput("message");
-    octokit.pulls.createComment({
-      owner: "chaos-monkeys",
-      repo: process.env.GITHUB_REPOSITORY,
-      pull_number: 10,
-      body: commentMessage,
-      commit_id: process.env.GITHUB_SHA,
-      path: process.env.GITHUB_EVENT_PATH
-    });
-    core.debug(`Action outputs message ${commentMessage}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-}
+// async function run() {
+//   checkRequiredEnv();
+//   try {
+//     const commentMessage = core.getInput("message");
+//     octokit.pulls.createComment({
+//       owner: "chaos-monkeys",
+//       repo: process.env.GITHUB_REPOSITORY,
+//       pull_number: 10,
+//       body: commentMessage,
+//       commit_id: process.env.GITHUB_SHA,
+//       path: process.env.GITHUB_EVENT_PATH
+//     });
+//     core.debug(`Action outputs message ${commentMessage}`);
+//   } catch (error) {
+//     core.setFailed(error.message);
+//   }
+// }
 
 run();
