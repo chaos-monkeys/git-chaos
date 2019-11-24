c674408bcb0bb7d85ac430ae6b5293ef7626512c
robalaban <rob.balaban@gmail.com>
2019-11-24 15:43:59 +0000

    Add refactor plus test branding

--- packages/backend/action/main.js
+++ packages/backend/action/main.js
@@ -7,43 +7,26 @@ const octokit = new Octokit({
   auth: GITHUB_TOKEN
 });
 
-// const checkRequiredEnv = () => {
-//   if (GITHUB_TOKEN === null && GITHUB_TOKEN === undefined) {
-//     core.warning(`Variable check failed: GITHUB_TOKEN: ${GITHUB_TOKEN}`);
-//     core.setFailed(error.message);
-//   }
-// };
+const checkRequiredEnv = () => {
+  if (GITHUB_TOKEN === null && GITHUB_TOKEN === undefined) {
+    core.warning(`Variable check failed: GITHUB_TOKEN: ${GITHUB_TOKEN}`);
+    core.setFailed(error.message);
+  }
+};
 
 const run = () => {
+  checkRequiredEnv();
   const commentMessage = core.getInput("message");
   octokit.issues
     .createComment({
       owner: "chaos-monkeys",
       repo: "git-chaos",
       issue_number: 10,
-      body: "Hello from Citizen M"
+      body: commentMessage
     })
     .catch(err => {
       console.log(err);
     });
 };
-
-// async function run() {
-//   checkRequiredEnv();
-//   try {
-//     const commentMessage = core.getInput("message");
-//     octokit.pulls.createComment({
-//       owner: "chaos-monkeys",
-//       repo: process.env.GITHUB_REPOSITORY,
-//       pull_number: 10,
-//       body: commentMessage,
-//       commit_id: process.env.GITHUB_SHA,
-//       path: process.env.GITHUB_EVENT_PATH
-//     });
-//     core.debug(`Action outputs message ${commentMessage}`);
-//   } catch (error) {
-//     core.setFailed(error.message);
-//   }
-// }
 
 run();
