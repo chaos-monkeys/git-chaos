f20d200a1e3b1d419ed6708ba774568407207bf1
robalaban <rob.balaban@gmail.com>
2019-12-08 16:03:22 +0000

    Add function to connect to bucket

--- packages/functions/data_parser.py
+++ packages/functions/data_parser.py
@@ -1,7 +1,24 @@
 import json
+import boto3
+
+s3 = boto3.client('s3')
 
 
 def lambda_handler(event, context):
+
+    bucket = 'test_bucket'
+    key = 'data/sample_data.json'
+
+    try:
+        data = s3.get_object(Bucket=bucket, Key=key)
+        json_data = data['Body'].read()
+
+        return json_data
+
+    except Exception as e:
+        print(e)
+        raise e
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello world Lambda!')
