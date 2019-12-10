efc833cbdacf669eaa5fa425d8e5fd6c57b6986a
Ollie Williams <3811773+pxlprfct@users.noreply.github.com>
2019-12-10 17:34:10 +0000

    Init action rework (#12)
    * added branch puller from github action

---
+++ packages/backend/init-action/main.js
@@ -0,0 +1,51 @@
+const core = require('@actions/core');
+const Octokit = require('@octokit/rest');
+const { getCodeHistory, getBranchName } = require('./helpers/history');
+const { createComment } = require('./helpers/comment');
+
+// these envs come from the github action
+const {
+  GITHUB_TOKEN,
+  GITHUB_SHA,
+  GITHUB_REPOSITORY,
+  GITHUB_REF,
+} = process.env;
+const [GIT_OWNER, GIT_REPO] = GITHUB_REPOSITORY.split('/');
+const issueNumber = GITHUB_REF.split('/')[2];
+
+
+const run = async () => {
+  core.debug(`issue_number: ${issueNumber}`);
+
+  const octokit = new Octokit({
+    auth: GITHUB_TOKEN,
+  });
+
+  const history = await getCodeHistory({
+    octokit,
+    owner: GIT_OWNER,
+    repo: GIT_REPO,
+    // TODO: might be easier with GITHUB_REF?
+    branch: await getBranchName({
+      octokit,
+      owner: GIT_OWNER,
+      repo: GIT_REPO,
+      sha: GITHUB_SHA,
+    }),
+  });
+
+  // TODO:add S3 upload here
+
+  core.debug(JSON.stringify(history));
+
+  return createComment({
+    octokit,
+    owner: GIT_OWNER,
+    repo: GIT_REPO,
+    issue_number: issueNumber,
+    // TODO: update with something less 'temporary'
+    message: 'Hey Rob!',
+  });
+};
+
+run();
