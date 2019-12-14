efed9d53511936215a4e9b5f2f2b4339aed440b6
Ollie Williams <ollie.williams@ibm.com>
2019-12-14 21:00:50 +0000

    commented out all code except AWS to check if race condition

--- packages/backend/init-action/main.js
+++ packages/backend/init-action/main.js
@@ -1,60 +1,58 @@
-const core = require('@actions/core');
-const Octokit = require('@octokit/rest');
-const { getCodeHistory, getBranchName } = require('./helpers/history');
-const { createComment } = require('./helpers/comment');
+// const core = require('@actions/core');
+// const Octokit = require('@octokit/rest');
+// const { getCodeHistory, getBranchName } = require('./helpers/history');
+// const { createComment } = require('./helpers/comment');
 const { uploadHistory } = require('./helpers/aws');
 
 
 // these envs come from the github action
 const {
-  GITHUB_TOKEN,
+  // GITHUB_TOKEN,
   GITHUB_SHA,
-  GITHUB_REPOSITORY,
-  GITHUB_REF,
+  // GITHUB_REPOSITORY,
+  // GITHUB_REF,
   AWS_ACCESS_KEY,
   AWS_SECRET_KEY,
 } = process.env;
-const [GIT_OWNER, GIT_REPO] = GITHUB_REPOSITORY.split('/');
-const issueNumber = GITHUB_REF.split('/')[2];
+// const [GIT_OWNER, GIT_REPO] = GITHUB_REPOSITORY.split('/');
+// const issueNumber = GITHUB_REF.split('/')[2];
 
 
 const run = async () => {
-  core.debug(`issue_number: ${issueNumber}`);
+  // const octokit = new Octokit({
+  //   auth: GITHUB_TOKEN,
+  // });
 
-  const octokit = new Octokit({
-    auth: GITHUB_TOKEN,
-  });
+  // const history = await getCodeHistory({
+  //   octokit,
+  //   owner: GIT_OWNER,
+  //   repo: GIT_REPO,
 
-  const history = await getCodeHistory({
-    octokit,
-    owner: GIT_OWNER,
-    repo: GIT_REPO,
+  //   branch: await getBranchName({
+  //     octokit,
+  //     owner: GIT_OWNER,
+  //     repo: GIT_REPO,
+  //     sha: GITHUB_SHA,
+  //   }),
+  // });
 
-    branch: await getBranchName({
-      octokit,
-      owner: GIT_OWNER,
-      repo: GIT_REPO,
-      sha: GITHUB_SHA,
-    }),
-  });
-
-  core.debug(JSON.stringify(history));
+  // core.debug(JSON.stringify(history));
 
   await uploadHistory({
     accessKeyId: AWS_ACCESS_KEY,
     secretAccessKey: AWS_SECRET_KEY,
-    body: history,
+    body: { test: 'test' },
     sha: GITHUB_SHA,
   });
 
-  return createComment({
-    octokit,
-    owner: GIT_OWNER,
-    repo: GIT_REPO,
-    issue_number: issueNumber,
-    // TODO: update with something less 'temporary'
-    message: 'Hey Rob!',
-  });
+  // return createComment({
+  //   octokit,
+  //   owner: GIT_OWNER,
+  //   repo: GIT_REPO,
+  //   issue_number: issueNumber,
+  //   // TODO: update with something less 'temporary'
+  //   message: 'Hey Rob!',
+  // });
 };
 
 run();
