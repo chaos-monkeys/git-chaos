b2de2bfb79c1eb0ea50d917c81a02ef08bb3de22
Rober Balaban <robert.balaban@ibm.com>
2019-12-19 18:22:57 +0200

    Add jupyter notebook

--- packages/functions/data_parser.py
+++ packages/functions/data_parser.py
@@ -1,7 +1,18 @@
 import json
 
+# TODO: Find all authors in the Pull Request
+
+
+def extract_authors(data):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('returned from extract_authors data')
+    }
+
 
 def lambda_handler(event, context):
+    print('event', event)
+    print('context', context)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello world Lambda!')
