5300d642dfe7a9bb3a638ca4a8a2ea057f4d3b4d
Ollie Williams <ollie.williams@ibm.com>
2019-12-21 03:56:38 +0000

    added 'home' button to git page

--- packages/frontend/src/pages/git.js
+++ packages/frontend/src/pages/git.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Router } from "@reach/router";
 import Layout from "../components/layout";
+import Nav from "../templates/nav";
 import Route from "../wip/route";
 
 const Git = () => (
   <Layout>
+    <Nav />
     <h1>Git</h1>
     <Router>
       <Route path="git/:sha" />
