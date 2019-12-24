8d5f9dcebdbfb5f382248549e3635a78c2e4d2a1
robalaban <rob.balaban@gmail.com>
2019-12-24 14:08:00 +0200

    Removed debugger, retrying test

--- packages/backend/init-action/main.js
+++ packages/backend/init-action/main.js
@@ -48,8 +48,6 @@ const run = async () => {
     history: history,
   };
 
-  core.debug(`response_builder meta: ${reponseBuilder.meta}`);
-
   const path = await uploadHistory({
     accessKeyId: AWS_ACCESS_KEY,
     secretAccessKey: AWS_SECRET_KEY,
