7ff56bf99c3899cb085e958aef9ebf1b5e49c8fb
robalaban <rob.balaban@gmail.com>
2020-01-21 22:04:49 +0000

    Compiled new version

--- packages/backend/init-action/dist/index.js
+++ packages/backend/init-action/dist/index.js
@@ -9058,7 +9058,7 @@ const run = async () => {
       start_time: getCurrentTimestamp(),
       commit_sha: GITHUB_SHA
     },
-    collaborators: getCollaborators(),
+    collaborators: getCollaborators(octokit, owner, repo),
     historyIndex: historyIndex,
     history: history
   };
