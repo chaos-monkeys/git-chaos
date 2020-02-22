5fd076524441765aafc266571d1a7b88fb7d6c5c
robalaban <rob.balaban@gmail.com>
2020-02-22 19:22:18 +0000

    feat: add parsers module to feed from S3

--- Gitchaos/gc.backend/backend/collaborators/views.py
+++ Gitchaos/gc.backend/backend/collaborators/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Collaborator
+from .serializers import CollaboratorSerializer
+
+
+class CollaboratorList(generics.ListCreateAPIView):
+    """
+    List all projects, or create a new project.
+    """
+    queryset = Collaborator.objects.all()
+    serializer_class = CollaboratorSerializer
+
+
+class CollaboratorDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a project.
+    """
+    queryset = Collaborator.objects.all()
+    serializer_class = CollaboratorSerializer
+
